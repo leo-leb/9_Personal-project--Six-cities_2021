65a66d76d694deb4a02ec0c19b2e7557bccbd4f1
leo-leb <swan.leon.id@gmail.com>
2021-02-18 19:11:57 +0300

    3.1-Устранение-замечаний-наставника

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,12 @@ import FavoritesScreen from '../favorites-screen/favorites-screen';
 import RoomScreen from '../room-screen/room-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 
+const Routes = {
+  MAIN: `/`,
+  FAVORITES: `/favorites`,
+  SIGNIN: `/login`,
+  ROOM: `/offer/:id?`
+};
 
 const App = (props) => {
   const {places} = props;
@@ -14,16 +20,16 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={Routes.MAIN}>
           <MainScreen places={places}/>
         </Route>
-        <Route exact path="/favorites">
+        <Route exact path={Routes.FAVORITES}>
           <FavoritesScreen />
         </Route>
-        <Route exact path="/login">
+        <Route exact path={Routes.SIGNIN}>
           <SignInScreen />
         </Route>
-        <Route exact path="/offer/:id?">
+        <Route exact path={Routes.ROOM}>
           <RoomScreen />
         </Route>
         <Route>
